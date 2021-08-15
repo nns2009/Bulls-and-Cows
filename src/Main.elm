c52976d7e4ea887bb4a4a7c687a8d12534cdefd0
Igor Konyakhin <nns2009@yandex.ru>
2021-08-16 02:09:43 +0400

    Added "Rules" panel
    which opens and closes pretty smoothly

--- src/Main.elm
+++ src/Main.elm
@@ -7,14 +7,16 @@ import Random
 import Browser
 
 import Css exposing (..)
+import Css.Transitions as Tr
+import Css.Transitions exposing (transition)
+import Css.Global as GSS exposing (children)
 -- import Html
 import Html.Styled as H
-import Html.Styled exposing (node, text, div, th, tr, td, input, button, Html, toUnstyled)
+import Html.Styled exposing (text, div, span, p, th, tr, td, ul, li, input, button, form, Html, toUnstyled)
 import Html.Styled.Attributes as Attr
-import Html.Styled.Attributes exposing (value, type_, css, href, src)
+import Html.Styled.Attributes exposing (id, class, value, type_, css)
 import Html.Styled.Events exposing (onClick, onInput, onSubmit)
 import Debug exposing (toString)
-import Html.Styled exposing (form)
 
 main : Program () Model Msg
 main = Browser.document
@@ -105,19 +107,49 @@ update msg model =
                 , Cmd.none
                 )
 
+fillAllSpace = batch
+    [ position absolute
+    , top zero
+    , bottom zero
+    , left zero
+    , right zero
+    ]
+
+centerChildren = batch
+    [ displayFlex
+    , alignItems center
+    , justifyContent center
+    ]
+
 mainFontFamily = "Calibri"
 mainFontSize = em 1.6
 
-mainText = batch
+mainTextStyle = batch
     [ fontSize mainFontSize
     , fontFamilies [ mainFontFamily ]
     ]
+
+listStyle = batch
+    [ paddingLeft (em 1) ]
 
 cellStyle = batch
     [ width (pct 33)
     , border3 (px 1) solid (rgb 150 150 150)
     , textAlign center
     ]
+
+rulesPanelOpenTime = 350
+rulesClosedWidth = px 100
+rulesOpenWidth = px 290
+rulesPanelPadding = em 1
+rulesTextStyle = batch
+    [ fontSize (em 1.2)
+    , fontFamilies [ mainFontFamily ]
+    ]
+
+
+-- Debug styles
+debugBackgroundColor = rgb 255 200 200
 
 guessView : List Int -> List Int -> Html Msg
 guessView answer guess =
@@ -143,11 +175,12 @@ view model =
     in
         { title = answerText
         , body =
-            [ div [css [displayFlex, alignItems center, justifyContent center, height (pct 100)]]
-                [ node "style" [] [ text """
-                    html { height: 100%; }
-                    body { height: 100%; }
-                    """ ]
+            [ div [css [centerChildren, height (pct 100)]]
+                [ GSS.global
+                  [ GSS.html [ height (pct 100) ]
+                  , GSS.body [ height (pct 100) ]
+                  , GSS.everything [ boxSizing borderBox ]
+                  ]
                 , div [css
                     [ width (px 250)
                     , border3 (px 1) solid (rgb 255 0 0)
@@ -155,7 +188,7 @@ view model =
                     , flexDirection column
                     ]]
                     [ H.table [ css
-                        [ mainText
+                        [ mainTextStyle
                         , borderCollapse collapse
                         ]]
                         <| tr []
@@ -172,13 +205,13 @@ view model =
                         , css
                             [ flex2 (num 1) (num 1)
                             , minWidth (px 0)
-                            , mainText
+                            , mainTextStyle
                             ]
                         ] []
                         , button
                             [ Attr.disabled ((String.length model.guess /= digitsCount) || ((model.guess |> String.toList |> List.Extra.unique |> List.length) /= digitsCount))
                             , css
-                            [ mainText
+                            [ mainTextStyle
                             , width (em 1.6)
                             ]
                             ] [ text "?" ] ]
@@ -186,6 +219,66 @@ view model =
                     , div []
                         [ text model.message ]
                     ]
+                , div [ css
+                  [ position absolute
+                  , overflow hidden
+                  , right zero
+                  , top zero
+                  , bottom zero
+                  , backgroundColor debugBackgroundColor
+                  , rulesTextStyle
+                  , transition
+                    [ Tr.width rulesPanelOpenTime
+                    ]
+                  , width rulesClosedWidth
+                  , children
+                    [ GSS.class "revealHeader"
+                        [ opacity (num 1)
+                        , visibility visible
+                        , padding rulesPanelPadding
+                        , transition
+                          [ Tr.opacity rulesPanelOpenTime
+                          , Tr.visibility rulesPanelOpenTime
+                          ]
+                        , centerChildren
+                        ]
+                    , GSS.class "revealContent"
+                        [ opacity (num 0)
+                        , padding rulesPanelPadding
+                        , width rulesOpenWidth
+                        , transition
+                          [ Tr.opacity rulesPanelOpenTime
+                          ]
+                        , centerChildren 
+                        ]
+                    ]
+                  , hover
+                    [ width rulesOpenWidth
+                    , children
+                      [ GSS.class "revealHeader"
+                          [ opacity (num 0)
+                          , visibility hidden
+                          ]
+                      , GSS.class "revealContent"
+                          [ opacity (num 1)
+                          ]
+                      ]
+                    ]
+                  ]]
+                  [ div [ class "revealHeader", css [fillAllSpace] ] [ text "Rules" ]
+                  , div [ class "revealContent", css [height (pct 100)] ]
+                    [ div []
+                        [ p [] [ text "Computer picked up a random number (with a specified number of digits) where all digits are different - your goal is to guess it" ]
+                        , p [] [ text "Your guess has to be a number with the same number of digits and digits in your number should be different as well. For each of your guesses computer replies with a number of \"Cows\" and \"Bulls\", where"]
+                        , ul [ css [listStyle] ]
+                            [ li [] [ text "\"Cows\" means the number of correct digits but in the wrong position"]
+                            , li [] [ text "\"Bulls\" means the number of correct digits in the correct position"]
+                            ]
+                        , p [] [ text "Try to guess computer's number in the fewest moves possible!" ]
+                        , p [] [ text "Post your results in the comments:)" ]
+                        ]
+                    ]
+                  ]
                 ] |> toUnstyled
             ]
         }
